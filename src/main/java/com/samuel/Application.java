387563041c8b661f312abfa0d5fdc872f57ad10b
Samuel Martinucci <samuelmartinucci2>
2022-04-15 12:14:34 +0000

    Initial commit

---
+++ src/main/java/com/samuel/Application.java
@@ -0,0 +1,10 @@
+package com.samuel;
+
+import io.micronaut.runtime.Micronaut;
+
+public class Application {
+
+    public static void main(String[] args) {
+        Micronaut.run(Application.class, args);
+    }
+}
